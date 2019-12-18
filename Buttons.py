de9efa12e921c732d26a875bdb24292efc3ef5d4
Berdash <55478605+Berdash@users.noreply.github.com>
2019-12-18 12:20:23 +0300

    Create Buttons.py

---
+++ Buttons.py
@@ -0,0 +1,43 @@
+import pygame
+pygame.init()
+class Button:
+
+    def create_button(self, surface, color, x, y, length, height, width, text, text_color):
+        surface = self.draw_button(surface, color, length, height, x, y, width)
+        surface = self.write_text(surface, text, text_color, length, height, x, y)
+        self.rect = pygame.Rect(x,y, length, height)
+        return surface
+
+    # здесь отрисовка текста в кнопочке
+    def write_text(self, surface, text, text_color, length, height, x, y):
+        font_size = int(length//len(text))
+        myFont = pygame.font.SysFont("Arial", font_size)
+        myText = myFont.render(text, 1, text_color)
+        surface.blit(myText, ((x+length/2) - myText.get_width()/2, (y+height/2) - myText.get_height()/2))
+        return surface
+
+    # здесь отрисовка кнопочки
+    def draw_button(self, surface, color, length, height, x, y, width):
+        for i in range(1,10):
+            s = pygame.Surface((length+(i*2),height+(i*2)))
+            s.fill(color)
+            alpha = (255/(i+2))
+            s.set_alpha(alpha)
+            pygame.draw.rect(s, color, (x-i,y-i,length+i,height+i), width)
+            surface.blit(s, (x-i,y-i))
+        pygame.draw.rect(surface, color, (x,y,length,height), 1)
+        pygame.draw.rect(surface, (200,140,160), (x,y,length,height), 1)
+        return surface
+
+    # проверка мыши
+    def pressed(self, mouse):
+        if mouse[0] > self.rect.topleft[0]:
+            if mouse[1] > self.rect.topleft[1]:
+                if mouse[0] < self.rect.bottomright[0]:
+                    if mouse[1] < self.rect.bottomright[1]:
+
+                        return True
+                    else: return False
+                else: return False
+            else: return False
+        else: return False
